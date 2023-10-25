7cf08b8d6d61d0bd86502fb2a6d85e4621a338f9
mishkaua <marishka.pichik@gmail.com>
2023-10-25 16:19:18 +0200

    Added form signup

--- signup.php
+++ signup.php
@@ -1,52 +1,86 @@
-<?php ?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      function my_password_hash($password){
+        $salt = uniqid();
+        $hash = crypt($password, $salt);
+        return $hash;
+    }
+
+  function get_data() {
+    $data = array();
+    if (!preg_match("/^[a-zA-Z-' ]*$/", $_POST["login"])) {
+      echo "Invalid login" . "\n";
+      return false;
+    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+      echo "Invalid email format" . PHP_EOL;
+      return false;
+    } elseif ($_POST["password"] !== $_POST["password_confirmation"]) {
+      echo "Password and password confirmation do not match" . PHP_EOL;
+      return false;
+    }
+    return array(
+      "username" => $_POST['login'],
+      "email" => $_POST['email'],
+      "password" => my_password_hash($_POST['password'])
+    );
+  }
+  $data = get_data();
+  var_dump($data);
+  if ($data!==false){
+    echo "User created";
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="my_shop_style.css">
-    <title>Create your account</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="my_shop_style.css">
+  <title>Create your account</title>
 </head>
 
 <body>
-<form class="form-signin">
+  <form class="form-signup" action="signup.php" method="post">
     <h1 class="h3 mb-3 col-sm-6 font-weight-normal text-center">Create your account</h1>
     <div class="form-group row mb-3">
-      <label for="inputLogin" class="col-sm-1 col-form-label">Login</label>
+      <label for="login" class="col-sm-1 col-form-label">Login</label>
       <div class="col-sm-6">
-      <input type="text" class="form-control" id="inputLogin" placeholder="Enter your login">
+        <input type="text" class="form-control" name="login" minlength="5" maxlength="20" placeholder="Enter your login">
       </div>
     </div>
 
     <div class="form-group row mb-3">
-    <label for="inputEmail" class="col-sm-1 col-form-label">Email</label>
+      <label for="email" class="col-sm-1 col-form-label">Email</label>
       <div class="col-sm-6">
-        <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email" required="">
+        <input type="email" class="form-control" name="email" placeholder="Enter your email" required>
       </div>
     </div>
 
-  <div class="form-group row mb-3">
-    <label for="inputPassword" class="col-sm-1 col-form-label">Password</label>
-    <div class="col-sm-6">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Enter your password" required="">
+    <div class="form-group row mb-3">
+      <label for="password" class="col-sm-1 col-form-label">Password</label>
+      <div class="col-sm-6">
+        <input type="password" class="form-control" name="password" minlength="5" maxlength="20" placeholder="Enter your password" required>
+      </div>
     </div>
-  </div>
 
-  <div class="form-group row mb-3">
-    <label for="inputPasswordConfirm" class="col-sm-1 col-form-label">Password Confirmation</label>
-    <div class="col-sm-6">
-      <input type="password" class="form-control" id="inputPasswordConfirm" placeholder="Confirm your password" required="">
+    <div class="form-group row mb-3">
+      <label for="password_confirmation" class="col-sm-1 col-form-label">Password Confirmation</label>
+      <div class="col-sm-6">
+        <input type="password" class="form-control" name="password_confirmation" minlength="5" maxlength="20" placeholder="Confirm your password" required>
+      </div>
     </div>
-  </div>
 
     <div class="col-sm-6">
       <button type="submit" class="btn btn-dark">Register</button>
     </div>
-</form>
+  </form>
 
-<!--including basic Bootstrap scripts -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!--including basic Bootstrap scripts -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
