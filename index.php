9f65387c7e9ef8a8fe544064fed4e713ccedfa00
Razmo <you@example.com>
2023-10-25 11:20:58 +0200

    ADDING FOOTER

--- index.php
+++ index.php
@@ -251,8 +251,8 @@
 
         </div>
   </div>
-
-        <!--pagination-->
+<footer>
+   <!--pagination-->
         <nav aria-label="Page navigation example">
             <ul class="pagination">
               <li class="page-item"><a class="page-link" href="#">Previous</a></li>
@@ -263,6 +263,8 @@
             </ul>
           </nav>
   
+</footer>
+       
 
       <!--including basic Bootstrap scripts -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
