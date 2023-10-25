f5966163ab5af49694b1780290ff496fa7ed833f
mishkaua <marishka.pichik@gmail.com>
2023-10-25 17:34:10 +0200

    Corrected connection data

--- connect_db.php
+++ connect_db.php
@@ -3,12 +3,13 @@ function connect_db($host, $username, $password, $port, $db){
     $dsn = "mysql:dbname=$db;host=$host;port=$port";
     try { 
         $pdo = new PDO($dsn, $username, $password);
+        echo "Connection established".PHP_EOL;
         return $pdo;
     } 
     catch (PDOException $error) {
         echo "PDO Error: " . $error->getMessage() . PHP_EOL;
     }
-    echo "Connection established";
 }
 
-connect_db("127.0.01", "mm", "mm", "3306", "my_shop");
+$pdo = connect_db("127.0.0.1", "mm", "mm", "3306", "my_shop");
+var_dump($pdo);
