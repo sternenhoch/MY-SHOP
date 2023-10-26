0ef9643eef71189d31c18519f798b57f26e68501
mishkaua <marishka.pichik@gmail.com>
2023-10-26 12:25:04 +0200

    Replaced PHP_EOL by br

--- connect_db.php
+++ connect_db.php
@@ -3,13 +3,10 @@ function connect_db($host, $username, $password, $port, $db){
     $dsn = "mysql:dbname=$db;host=$host;port=$port";
     try { 
         $pdo = new PDO($dsn, $username, $password);
-        echo "Connection established".PHP_EOL;
+        echo "Connection established"."<br>";
         return $pdo;
     } 
     catch (PDOException $error) {
         echo "PDO Error: " . $error->getMessage() . PHP_EOL;
     }
 }
-
-$pdo = connect_db("127.0.0.1", "mm", "mm", "3306", "my_shop");
-var_dump($pdo);
