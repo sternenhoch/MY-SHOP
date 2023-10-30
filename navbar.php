3fbe707866d8d82f08078b775a711999cebe647d
Razmo <you@example.com>
2023-10-30 03:52:21 +0100

    ADDED: MODIFIED SEARCH FORM

--- navbar.php
+++ navbar.php
@@ -17,13 +17,27 @@
           <a class="nav-link" href="#">Blog</a>
         </li>
       </ul>
+
+      <?php 
+      /*if (isset($_SESSION['user_id'])) {
+        echo '<a class="btn btn-dark" href="./logout.php" role="button">Logout</a>';
+      } else {
+        echo '<a class="btn btn-dark" href="./signup.php" role="button">Create your account</a>';
+        echo '<a class="btn btn-dark" href="./signin.php" role="button">Sign In</a>';
+      }*/
+      ?>
       <a class="btn btn-dark" href="./signup.php" role="button">Create your account</a>
       <a class="btn btn-dark" href="./signin.php" role="button">Sign In</a>
       <a class="btn btn-dark" href="./logout.php" role="button">Logout</a>
+
     </div>
   </div>
-  <form class="d-flex" role="search">
+   <!--<form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-secondary" type="submit">Search</button>
+      </form> -->
+      <form class="d-flex" action="search.php" method="GET">
+        <input type ="text" name="query" placeholder="Search">
+        <input type="submit" value="Search">
       </form>
 </nav>
